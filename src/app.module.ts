b292a2c197bb2a98f1c37de1dacf33924a0cb72f
loveAlakazam <dmsrkd1216@gmail.com>
2022-11-24 21:55:24 +0900

    :wrench: init: 데이터베이스 모듈 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MysqlConfigModule } from './mysql/mysql.config.module';
 
 @Module({
-  imports: [],
+  imports: [MysqlConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
