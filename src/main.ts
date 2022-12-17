5422216059065291c6fd5af2a0c5b9e3c876a9b2
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-17 19:11:25 +0900

    chore: cors 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './error/http-exception.filter';
-import { AllExceptionsFilter } from './error/all-exception-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   // HttpExceptionFilter 적용
   app.useGlobalFilters(new HttpExceptionFilter());
