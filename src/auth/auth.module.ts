20d564c65a5d7da7493221aae83026c5c16b21f2
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-19 00:25:55 +0900

    chore: auth.module import 모듈추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
-import { UsersModule } from 'src/users/users.module';
-import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
+import { jwtConstants } from './jwt.constant';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '120s' },
+    }),
+  ],
   controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
