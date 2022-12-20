f46f73829dd8b5e39ed3ee5320589f7c68af4a82
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-20 15:28:04 +0900

    fix: cookie-parser 임포트문 수정

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './error/http-exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   // HttpExceptionFilter 적용
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // 쿠키적용
+  app.use(cookieParser());
 
   await app.listen(process.env.SERVER_PORT);
 }
