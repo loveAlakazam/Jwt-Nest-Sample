984f1f915d6019d1a06b2a5779638c8a55716f06
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-21 02:49:56 +0900

    chore: NotFoundUser 사용자정의 에러 추가

--- src/error/users/users-error-messages.ts
+++ src/error/users/users-error-messages.ts
@@ -3,4 +3,8 @@ export const UsersErrorMessages = {
     code: 401,
     message: '아이디와 비밀번호가 일치하지 않습니다.',
   },
+  NOT_FOUND_USER: {
+    code: 404,
+    message: '존재하지 않는 회원입니다.',
+  },
 };
--- src/error/users/users-exception.ts
+++ src/error/users/users-exception.ts
@@ -1,8 +1,14 @@
-import { UnauthorizedException } from '@nestjs/common';
+import { NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { UsersErrorMessages } from './users-error-messages';
 
 export class FailedValidate extends UnauthorizedException {
   constructor() {
     super(UsersErrorMessages.LOGIN_FAIL);
+  }
+}
+
+export class NotFoundUser extends NotFoundException {
+  constructor() {
+    super(UsersErrorMessages.NOT_FOUND_USER);
   }
 }
