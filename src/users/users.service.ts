506cb411c16a153a15b768f4d1bfe2dddb8b0dd1
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-21 02:12:05 +0900

    chore: 이메일검색, 유저생성 서비스 로직추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
+import { CreateUserRequestDto } from './dto/create-user-request.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async findAll() {
-    return await this.usersRepository.findAll();
+    return await this.usersRepository.getAll();
   }
 
   async findUserByName(name: string) {
-    return await this.usersRepository.findOneByName(name);
+    return await this.usersRepository.getByName(name);
+  }
+
+  async findUserByEmail(email: string) {
+    return await this.usersRepository.getByEmail(email);
+  }
+
+  async createNewUser(user: CreateUserRequestDto) {
+    return await this.usersRepository.createNewUser(user);
   }
 }
