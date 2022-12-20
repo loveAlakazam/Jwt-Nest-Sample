afaac8905a9719eeb7d86b23098043cacd7559d4
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-21 02:13:32 +0900

    chore: 유저생성 컨트롤러 로직 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { Public } from 'src/decorators/public.decorator';
+import { Public } from '../decorators/public.decorator';
+import { CreateUserRequestDto } from './dto/create-user-request.dto';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +11,11 @@ export class UsersController {
   @Get()
   async findAll() {
     return this.usersService.findAll();
+  }
+
+  @Public()
+  @Post()
+  async createNewUser(@Body() user: CreateUserRequestDto): Promise<void> {
+    await this.usersService.createNewUser(user);
   }
 }
