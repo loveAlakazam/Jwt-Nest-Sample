8690ec14d3f643fe03bac485c462446e130a6afd
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-22 04:51:24 +0900

    remove: 불필요 로직 제거

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,5 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
-}
+export class AppController {}
