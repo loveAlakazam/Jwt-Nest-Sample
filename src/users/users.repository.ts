8f2b48213b6b72c3239f6d6975887bde9bf7f09a
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-22 02:59:47 +0900

    feat: refreshToken 수정로직과 비밀번호제외 유저정보 로직추가

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UseFilters } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from '../entity/User.entity';
 import { CreateUserRequestDto } from './dto/create-user-request.dto';
+import { HttpExceptionFilter } from 'src/error/http-exception.filter';
+import { NotFoundUser } from 'src/error/users/users-exception';
 
+@UseFilters(new HttpExceptionFilter())
 @Injectable()
 export class UsersRepository {
   constructor(
@@ -23,11 +26,36 @@ export class UsersRepository {
     return await this.usersRepository.findOneBy({ id: id });
   }
 
+  async getUserInfoWithoutPassword(id: number) {
+    try {
+      const user = await this.usersRepository.findOneBy({ id: id });
+      const { password, ...withoutPassword } = user;
+
+      if (user) {
+        return withoutPassword;
+      }
+
+      throw new NotFoundUser();
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async getAll() {
     return await this.usersRepository.find();
   }
 
   async createNewUser(user: CreateUserRequestDto) {
     return await this.usersRepository.save(user);
+  }
+
+  async updateUserRefreshToken(id: number, refreshToken: string | null) {
+    return await this.usersRepository
+      .createQueryBuilder('users')
+      .update(Users)
+      .set({
+        refreshToken: refreshToken,
+      })
+      .where('id = :id', { id: id });
   }
 }
