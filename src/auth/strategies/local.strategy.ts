aa15a96ee2576d979507e0c7f0c648fd51437767
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-22 03:16:48 +0900

    fix: strategy이름 부여

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../auth.service';
 import { FailedValidate } from 'src/error/users/users-exception';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
   constructor(private authService: AuthService) {
     super({
       usernameField: 'email',
