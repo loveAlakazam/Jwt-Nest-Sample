89e801a91412867137bc737b036224b0a1b5d98d
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-22 04:51:57 +0900

    fix: 로그인/로그아웃/토큰갱신 내부로직 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,40 +1,85 @@
-import { Controller, Post, UseGuards, Req, Res, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Req,
+  Res,
+  Body,
+  Get,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { Public } from '../decorators/public.decorator';
 import { Users } from '../entity/User.entity';
+import { UsersService } from 'src/users/users.service';
+import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Req() req, @Res({ passthrough: true }) res: Response) {
-    // 토큰 발급
-    const token = await this.authService.login(req.user);
+    const user = req.user;
 
-    // 반환된 토큰값을 쿠키에 토큰을 저장
-    res.cookie('Authentication', token, {
-      domain: 'localhost',
-      path: '/',
-      httpOnly: true,
-      maxAge: 60 * 1000, // 1분
-    });
-    return this.authService.login(req.user);
+    // 쿠키에서 accessToken 발급 받는다.
+    const { accessToken, ...accessTokenOption } =
+      await this.authService.getCookieWithJwtAccessToken(user.id);
+
+    // 쿠키에서 refreshToken 발급 받는다.
+    const { refreshToken, ...refreshTOkenOption } =
+      await this.authService.getCookieWithJwtRefreshToken(user.id);
+
+    // refreshToken값을 db에 저장
+    await this.usersService.setRefreshToken(refreshToken, user.id);
+
+    res.cookie('Authentication', accessToken, accessTokenOption);
+    res.cookie('Refresh', refreshToken, refreshTOkenOption);
+
+    return user;
   }
 
+  @Public()
+  @UseGuards(JwtRefreshGuard)
   @Post('logout')
-  async logOut(@Res({ passthrough: true }) res: Response) {
-    const { token, ...option } = await this.authService.logout();
-    res.cookie('Authentication', token, option);
+  async logOut(@Req() req, @Res({ passthrough: true }) res: Response) {
+    const { accessTokenOption, refreshTokenOption } =
+      await this.authService.getCookiesForLogOut();
+
+    await this.usersService.removeRefreshToken(req.user.id);
+
+    // accessToken, refreshToken 값을 없앤다.
+    res.cookie('Authentication', '', accessTokenOption);
+    res.cookie('Refresh', '', refreshTokenOption);
   }
 
+  // 회원가입
   @Public()
   @Post('register')
   async register(@Body() user: Users) {
     return this.authService.register(user);
+  }
+
+  // 토큰 갱신
+  /**
+   * - JwtRefreshGuard에 의해 쿠키의 RefreshToken이 유저의 db테이블의 RefreshToken과 일치한지 확인
+   * - 이후에 AccessToken을 발급받는다.
+   */
+  @Public()
+  @UseGuards(JwtRefreshGuard)
+  @Get('refresh')
+  refresh(@Req() req, @Res({ passthrough: true }) res: Response) {
+    const user = req.user;
+    const { accessToken, ...accessTokenOption } =
+      this.authService.getCookieWithJwtAccessToken(user.id);
+
+    res.cookie('Authentication', accessToken, accessTokenOption);
+    return user;
   }
 }
