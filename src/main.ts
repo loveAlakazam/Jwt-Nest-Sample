1048ab1a4b4fec28c065be62ef9088425a9f2cb6
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-27 03:43:05 +0900

    fix: pipe적용 - ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './error/http-exception.filter';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  // global pipes
+  app.useGlobalPipes(new ValidationPipe());
 
   // HttpExceptionFilter 적용
   app.useGlobalFilters(new HttpExceptionFilter());
