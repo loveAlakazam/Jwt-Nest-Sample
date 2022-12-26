0aace095d2fb6d383123924ff497e01e2f5c0045
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-27 03:48:50 +0900

    fix: api 위치 변경 - param을 받는 api가 아래로 위치하도록 변경

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,31 @@
-import { Controller, Get, Param, ParseIntPipe, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { User } from 'src/decorators/user.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async findAll() {
+  async findAll(@User() user) {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  async findUserById(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findUserById(id);
+  @Get('my')
+  async getProfile(@User() user) {
+    return user;
   }
 
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
+  @Get(':id')
+  async findUserById(@Param('id', ParseIntPipe) id: number, @User() user) {
+    return this.usersService.findUserById(id);
   }
 }
