6118ee52eb5c9156ae37cf4bd947a5eae2cb440c
loveAlakazam <dmsrkd1216@gmail.com>
2023-01-11 22:39:35 +0900

    feat: google strategy 추가

---
+++ src/auth/strategies/google.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleLoginStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      clientID: configService.get('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
+      callbackURL: configService.get('GOOGLE_LOGIN_CALLBACK_URL'),
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ) {
+    const { name, emails } = profile;
+    const user = {
+      email: emails[0].value,
+      name: name.givenName,
+      accessToken,
+    };
+
+    done(null, user);
+  }
+}
