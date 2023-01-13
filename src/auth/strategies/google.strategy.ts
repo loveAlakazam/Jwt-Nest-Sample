59c45934b10fb1aeab53d7515cec44366eb2d0fb
loveAlakazam <dmsrkd1216@gmail.com>
2023-01-14 01:05:44 +0900

    feat: passport-google guard,strategy 생성

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { AuthService } from '../auth.service';
+import { UsersErrorMessages } from 'src/error/users/users-error-messages';
 
 @Injectable()
 export class GoogleLoginStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
     super({
       clientID: configService.get('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
@@ -14,19 +19,24 @@ export class GoogleLoginStrategy extends PassportStrategy(Strategy, 'google') {
     });
   }
 
-  async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: any,
-    done: VerifyCallback,
-  ) {
-    const { name, emails } = profile;
-    const user = {
-      email: emails[0].value,
-      name: name.givenName,
-      accessToken,
-    };
+  async validate(accessToken: string, refreshToken: string, profile: any) {
+    // accssToken: 구글서버로부터 인증 받은 access 토큰
+    // refreshToken: 구글서버로부터 인증 받은 refresh 토큰
+    // profile: 현재 연동 로그인하려는 구글 계정 프로필정보
 
-    done(null, user);
+    // 1. 구글 프로필의 값이 존재하는지 체크
+    if (!profile) {
+      throw new UnauthorizedException(UsersErrorMessages.ACCESS_GOOGLE_DENY);
+    }
+    const { id, emails, name } = profile;
+
+    // 2. '액세스토큰' + '리프래시 토큰' + '연동로그인된 회원정보'  를 가져온다.
+    const user = await this.authService.validateGoogle({
+      name: name.givenName,
+      email: emails[0].value,
+      googleAccount: id,
+    });
+
+    return user;
   }
 }
