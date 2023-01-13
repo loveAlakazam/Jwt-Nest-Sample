5717d244416729196d762804d7a4ecbf07ebd674
loveAlakazam <dmsrkd1216@gmail.com>
2023-01-14 01:08:19 +0900

    fix: User 엔티티 필드  추가

--- src/entity/User.entity.ts
+++ src/entity/User.entity.ts
@@ -1,5 +1,4 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-import { Exclude } from 'class-transformer';
 
 @Entity({ schema: 'jwt_test', name: 'users' })
 export class Users {
@@ -12,10 +11,45 @@ export class Users {
   @Column({ unique: true })
   email: string;
 
-  @Column()
+  @Column({ nullable: true })
   password: string;
 
-  @Column({ nullable: true, default: null, comment: '암호화된 refresh 토큰' })
-  @Exclude()
+  //구글
+  @Column({ nullable: true, default: null })
+  googleAccount: string;
+
+  // naver
+  @Column({ nullable: true, default: null })
+  naverAccount: string;
+
+  // refresh token
+  @Column({
+    type: 'text',
+    nullable: true,
+    default: null,
+    comment: '암호화된 refresh 토큰',
+  })
   refreshToken?: string;
+
+  // 회원가입시점
+  @Column({ type: 'timestamp', default: () => 'now()' })
+  joinedAt: Date;
+
+  // 회원탈퇴시점
+  @Column({
+    type: 'timestamp',
+    default: null,
+    comment: '회원탈퇴한 회원일때 탈퇴날짜 데이터저장',
+  })
+  withdrawAt: Date;
+
+  // verify-Email
+  @Column({ default: false, comment: '이메일 인증' })
+  emailVerify: boolean;
+
+  @Column({ nullable: true, default: null })
+  phoneNumber?: string;
+
+  @Column({ default: false, comment: 'SMS 휴대전화번호 인증' })
+  phoneVerify: boolean;
 }
