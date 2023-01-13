29d4d15a67ecd0a7227f2c0b6575f9bf115a7747
loveAlakazam <dmsrkd1216@gmail.com>
2023-01-14 00:59:54 +0900

    fix: 유저 cru 데이터베이스 orm로직 수정

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -2,9 +2,10 @@ import { Injectable, UseFilters } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from '../entity/User.entity';
-import { CreateUserRequestDto } from './dto/create-user-request.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { HttpExceptionFilter } from 'src/error/http-exception.filter';
-import { NotFoundUser } from 'src/error/users/users-exception';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { SocialUserDto } from './interfaces/social-user.interface';
 
 @UseFilters(new HttpExceptionFilter())
 @Injectable()
@@ -19,44 +20,54 @@ export class UsersRepository {
   }
 
   async getByEmail(email: string) {
-    return await this.usersRepository.findOneBy({ email: email });
+    // 로그인 입력정보와 데이터베이스에 저장된 회원정보가 일치한지 확인하기 위해서 비밀번호도 같이
+    const user = await this.usersRepository.findOneBy({ email: email });
+    return user;
   }
 
   async getById(id: number) {
-    return await this.usersRepository.findOneBy({ id: id });
-  }
-
-  async getUserInfoWithoutPassword(id: number) {
-    try {
-      const user = await this.usersRepository.findOneBy({ id: id });
-      const { password, ...withoutPassword } = user;
-
-      if (user) {
-        return withoutPassword;
-      }
-
-      throw new NotFoundUser();
-    } catch (error) {
-      throw error;
-    }
+    const user = await this.usersRepository.findOneBy({ id: id });
+    return user;
   }
 
   async getAll() {
     return await this.usersRepository.find();
   }
 
-  async createNewUser(user: CreateUserRequestDto) {
-    return await this.usersRepository.save(user);
+  async create(createUserDto: CreateUserDto) {
+    return await this.usersRepository.save(createUserDto);
   }
 
-  async updateUserRefreshToken(id: number, refreshToken: string | null) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return await this.usersRepository
       .createQueryBuilder('users')
       .update(Users)
-      .set({
-        refreshToken: refreshToken,
-      })
+      .set({ ...updateUserDto })
       .where('id = :id', { id: id })
       .execute();
   }
+
+  async remove(id: number) {
+    return await this.usersRepository.delete(id);
+  }
+
+  async createUserFromSocial(createSocialUserDto: SocialUserDto) {
+    await this.usersRepository
+      .createQueryBuilder('users')
+      .insert()
+      .into(Users)
+      .values({ ...createSocialUserDto })
+      .execute();
+  }
+
+  // async updateUserRefreshToken(id: number, refreshToken: string | null) {
+  //   return await this.usersRepository
+  //     .createQueryBuilder('users')
+  //     .update(Users)
+  //     .set({
+  //       refreshToken: refreshToken,
+  //     })
+  //     .where('id = :id', { id: id })
+  //     .execute();
+  // }
 }
