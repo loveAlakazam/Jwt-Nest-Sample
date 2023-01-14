0c477281f69ffb63a95834fe15188b0b3c8637cc
loveAlakazam <dmsrkd1216@gmail.com>
2023-01-15 04:15:07 +0900

    fix: refreshToken strategy 내부로직 수정

--- src/auth/strategies/jwt-refresh.strategy.ts
+++ src/auth/strategies/jwt-refresh.strategy.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersErrorMessages } from '../../error/users/users-error-messages';
 import { AuthService } from '../auth.service';
-import { Request as RequestType } from 'express';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(
@@ -15,25 +20,36 @@ export class JwtRefreshStrategy extends PassportStrategy(
     private readonly authService: AuthService,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        JwtRefreshStrategy.extractJWT,
-        ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ]),
-      secretOrKey: configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
       passReqToCallback: true,
+      secretOrKey: configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          const refreshToken = request?.cookies?.refreshToken;
+          if (!refreshToken) return null;
+
+          return refreshToken;
+        },
+      ]),
     });
   }
 
-  async validate(req, payload: any) {
-    // RefreshToken이 유효한지 확인후에 유저정보를 반환.
-    const refreshToken = req.cookies?.refresh_token;
-    return this.authService.checkVerifyRefreshToken(refreshToken, payload.id);
-  }
-
-  private static extractJWT(req: RequestType): string | null {
-    if (req.cookies && req.cookies.refresh_token) {
-      return req.cookies.refresh_token;
+  async validate(req: Request, payload: any) {
+    // 토큰갱신할 때
+    // refreshToken이 쿠키에 존재하는지 확인
+    const refreshToken = req.cookies?.refreshToken;
+    if (!refreshToken) {
+      throw new UnauthorizedException(UsersErrorMessages.NOT_FOUND_TOKEN);
     }
-    return null;
+
+    // 요청유저가 존재하는지 확인
+    if (!payload) {
+      throw new NotFoundException(UsersErrorMessages.NOT_FOUND_USER);
+    }
+
+    // 새로발급받은 access토큰, refresh 토큰을 리턴한다.
+    return await this.authService.validateRefreshToken(
+      payload.sub,
+      refreshToken,
+    );
   }
 }
