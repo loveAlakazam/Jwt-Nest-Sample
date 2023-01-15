067a728910e9243c2b49ce785ad5329d2468f257
loveAlakazam <dmsrkd1216@gmail.com>
2023-01-16 00:11:00 +0900

    fix: 도메인명 수정 및 유저프로필, 유저조회 api 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,6 +9,8 @@ import {
   Delete,
   HttpCode,
   UseFilters,
+  Param,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
@@ -23,18 +25,20 @@ import { HttpExceptionFilter } from '../error/http-exception.filter';
 import { GoogleAuthGuard } from './guards/google-auth.guard';
 import { KakaoAuthGuard } from './guards/kakao-auth.guard';
 import { NaverAuthGuard } from './guards/naver-auth.guard';
+import { UsersService } from '../users/users.service';
 
 @ApiTags('Auth')
 @UseFilters(HttpExceptionFilter)
-@Controller('auth')
+@Controller()
 export class AuthController {
   constructor(
     private readonly configService: ConfigService,
     private readonly authService: AuthService,
+    private readonly usersService: UsersService,
   ) {}
 
   @ApiOperation({ summary: '회원가입 API' })
-  @Post('register')
+  @Post('auth/register')
   async register(@Body() data: CreateUserDto) {
     return await this.authService.register(data);
   }
@@ -42,7 +46,7 @@ export class AuthController {
   @HttpCode(200)
   @ApiOperation({ summary: '로그인 API' })
   @UseGuards(LocalAuthGuard) // 로그인할때 입력정보가 올바른지 검증하는 가드
-  @Post('login')
+  @Post('auth/login')
   async signIn(
     @User() user,
     @Req() req: Request,
@@ -60,11 +64,14 @@ export class AuthController {
     };
   }
 
-  @HttpCode(200)
   @ApiOperation({ summary: '로그아웃 API' })
+  @HttpCode(200)
   @UseGuards(JwtAuthGuard)
-  @Get('logout')
+  @Get('auth/logout')
   async signOut(@User() user, @Req() req: Request, @Res() res: Response) {
+    console.log(user);
+    console.log(req.user);
+    console.log(req.cookies);
     // 1. refreshToken값을 null 로 변경한다.
     await this.authService.signOut(user.id);
 
@@ -80,7 +87,7 @@ export class AuthController {
     summary: '구글 로그인 - googleAuthGuard로부터 로그인 리다이렉션 URL을 호출',
   })
   @UseGuards(GoogleAuthGuard)
-  @Get('google')
+  @Get('auth/google')
   async GoogleLogin(@Req() req: Request) {
     return;
   }
@@ -89,7 +96,7 @@ export class AuthController {
     summary: '구글로그인 리다이렉션',
   })
   @UseGuards(GoogleAuthGuard)
-  @Get('google/redirect')
+  @Get('auth/google/redirect')
   async GoogleLoginRedirect(
     @User() user,
     @Req() req: Request,
@@ -106,7 +113,7 @@ export class AuthController {
       '카카오 로그인 요청 - kakaoAuthGuard로부터 로그인 리다이렉션 URL을 호출',
   })
   @UseGuards(KakaoAuthGuard)
-  @Get('kakao')
+  @Get('auth/kakao')
   async KakaoLogin(@Req() req: Request) {
     return;
   }
@@ -115,7 +122,7 @@ export class AuthController {
     summary: '카카오 로그인 리다이렉션',
   })
   @UseGuards(KakaoAuthGuard)
-  @Get('kakao/redirect')
+  @Get('auth/kakao/redirect')
   async KakaoLoginRedirect(
     @User() user,
     @Req() req: Request,
@@ -132,7 +139,7 @@ export class AuthController {
       '네이버 로그인 요청 - naverAuthGuard로부터 로그인 리다이렉션 URL을 호출',
   })
   @UseGuards(NaverAuthGuard)
-  @Get('naver')
+  @Get('auth/naver')
   async NaverLogin(@Req() req: Request) {
     return;
   }
@@ -141,7 +148,7 @@ export class AuthController {
     summary: '네이버 로그인 리다이렉션',
   })
   @UseGuards(NaverAuthGuard)
-  @Get('naver/redirect')
+  @Get('auth/naver/redirect')
   async NaverLoginRedirect(
     @User() user,
     @Req() req: Request,
@@ -150,5 +157,22 @@ export class AuthController {
     res.cookie('accessToken', user.accessToken, { httpOnly: true });
     res.cookie('refreshToken', user.refreshToken, { httpOnly: true });
     return { message: 'Login with Naver Success!', ...user };
+  }
+
+  // 로그인한 회원정보 리턴
+  // /my
+  @ApiOperation({ summary: '로그인 유저정보 조회 API' })
+  @UseGuards(JwtAuthGuard)
+  @Get('my')
+  async getProfile(@User() user) {
+    return user;
+  }
+
+  // 회원별 회원정보 리턴
+  @ApiOperation({ summary: '회원정보 리턴' })
+  @Get(':id')
+  @ApiOperation({ summary: '로그인 유저 아이디 조회 API' })
+  async findUserById(@Param('id', ParseIntPipe) id: number, @User() user) {
+    return this.usersService.findUserById(id);
   }
 }
