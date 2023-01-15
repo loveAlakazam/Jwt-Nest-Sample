89efe940ac1e66683fc553ae406dc792edaa2468
loveAlakazam <dmsrkd1216@gmail.com>
2023-01-16 00:14:39 +0900

    fix: UsersController 제거

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../entity/User.entity';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
-  controllers: [UsersController],
+  controllers: [],
   providers: [UsersService, UsersRepository],
 
   /** auth 모듈에서 jwt 인증을 하기위해 UsersService가 필요함 */
