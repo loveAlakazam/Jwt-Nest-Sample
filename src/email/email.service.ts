5f6ed382a16ef2dbd9ba9c21706dab46ecff6501
eunkangchoi <ekk12mv2@gmail.com>
2023-01-20 13:51:14 +0900

    chore: 테스트 메일 전송 임시 api 작성

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,3 +1,4 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable, UseFilters } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from '../error/http-exception.filter';
@@ -5,5 +6,27 @@ import { HttpExceptionFilter } from '../error/http-exception.filter';
 @UseFilters(new HttpExceptionFilter())
 @Injectable()
 export class EmailService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly mailerService: MailerService,
+  ) {}
+
+  async sendHello(): Promise<boolean> {
+    await this.mailerService
+      .sendMail({
+        to: 'dmsrkd1216@gmail.com',
+        from: 'noreply@gmail.com',
+        subject: 'Hello',
+        text: 'hello World',
+        html: `<b>Hello World</b>`,
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+        throw error;
+      });
+    return true;
+  }
 }
