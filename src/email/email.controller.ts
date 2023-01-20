fe4d4a140194aa015fdce7c0fb6e16b005d8bf11
eunkangchoi <ekk12mv2@gmail.com>
2023-01-20 13:51:14 +0900

    chore: 테스트 메일 전송 임시 api 작성

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,11 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EmailService } from './email.service';
 
-@Controller()
+@Controller('email')
 export class EmailController {
   constructor(
     private readonly emailService: EmailService,
     private readonly configService: ConfigService,
   ) {}
+
+  @Get()
+  async testEmail() {
+    return this.emailService.sendHello();
+  }
 }
