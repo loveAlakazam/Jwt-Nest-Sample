32e9d19fcabd2efcddf214c4eb28fb6813ae9ade
eunkangchoi <ekk12mv2@gmail.com>
2023-01-20 13:50:26 +0900

    chore: Mailer모듈 dependency 추가

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,11 +1,42 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: 'smtp.example.com',
+          port: 587, // Single Connection
+          secure: false, // Use TLS
+          auth: {
+            user: configService.get<string>('MAILER_USER'),
+            pass: configService.get<string>('MAILER_PWD'),
+          },
+        },
+        defaults: {
+          from: '"No-Reply" <no-reply@localhost>',
+        },
+        preview: true,
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
   controllers: [EmailController],
   providers: [EmailService],
+  exports: [EmailService],
 })
 export class EmailModule {}
