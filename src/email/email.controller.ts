998fdbb9287c1e51fe5abe88f1f1c395d6503be5
loveAlakazam <dmsrkd1216@gmail.com>
2023-01-25 19:20:06 +0900

    chore: email 서비스 일부내부로직 수정

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EmailService } from './email.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('email')
 export class EmailController {
@@ -9,8 +10,19 @@ export class EmailController {
     private readonly configService: ConfigService,
   ) {}
 
-  @Get()
+  @ApiOperation({
+    summary: '메일전송 테스트',
+  })
+  @Get('test')
   async testEmail() {
-    return this.emailService.sendHello();
+    return await this.emailService.sendHello();
+  }
+
+  @ApiOperation({
+    summary: '이메일 인증번호 인증',
+  })
+  @Post('auth')
+  async authenticateByEmail() {
+    return;
   }
 }
