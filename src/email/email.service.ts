eca84cfcb2bb4fb0b52e194badfda05f6420093e
loveAlakazam <dmsrkd1216@gmail.com>
2023-02-09 21:11:38 +0900

    fix: 요청 dto추가하여 함수내부 수정

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -2,6 +2,8 @@ import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable, UseFilters } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from '../error/http-exception.filter';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { ReceiveEmailRequestDto } from './dto/receive-email-request.dto';
 
 @UseFilters(new HttpExceptionFilter())
 @Injectable()
@@ -9,24 +11,36 @@ export class EmailService {
   constructor(
     private readonly configService: ConfigService,
     private readonly mailerService: MailerService,
+    private readonly eventEmitter: EventEmitter2,
   ) {}
 
-  async sendHello(): Promise<boolean> {
-    await this.mailerService
-      .sendMail({
-        to: 'ekk12mv2@gmail.com',
-        from: 'noreply@gmail.com',
-        subject: 'Hello test',
-        text: 'hello World!!!',
-        html: `<b>hello~~~</b>`,
-      })
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.error(error);
-        throw error;
-      });
-    return true;
+  async sendHello(requestDto: ReceiveEmailRequestDto) {
+    try {
+      await this.mailerService
+        .sendMail({
+          to: requestDto.email,
+          from: 'noreply@gmail.com',
+          subject: 'Hello test',
+          text: 'hello World!!!',
+          html: `<b>${requestDto.content} hello~~~</b>`,
+        })
+        .then((result) => {
+          return true;
+        })
+        .catch((error) => {
+          console.error(error);
+          throw error;
+        });
+      return true;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  @OnEvent('mail_greeting_newbie', { async: true })
+  public async handleMailGreetingNewbie() {
+    // 가입자에게 이메일 전송한다.
+    return;
   }
 }
